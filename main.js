7e0397e5ce2eb08a04d7667fd3abc6cd93d20bf5
sofia-lang <soc_16@hotmail.com>
2019-09-30 20:03:51 -0600

    primer commit repo

---
+++ main.js
@@ -0,0 +1,22 @@
+//let nombre1= "Juan";
+//console.log (nombre1);
+//let nombre2= "Sofia";
+//console.log (nombre2);
+//let nombre3="Susi";
+//console.log (nombre3);
+
+//let nombre1= prompt("Digite su nombre:");
+//console.log(nombre1);
+//let nombre2= prompt("Digite su nombre:");
+//console.log(nombre2);
+//let nombre3= prompt("Digite su nombre:")
+//console. log(nombre3);
+
+let nombre1= prompt("Digite su nombre:");
+let apellido1= prompt("Digite su apellido:")
+
+function nombrecompleto (nombre1, apelledo1){
+return "Su nombre completo es: "+nombre1 + " " + apellido1;
+}
+console.log (nombrecompleto(nombre1,apellido1));
+
